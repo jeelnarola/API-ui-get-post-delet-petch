d2e5d88ca5fa7feaca93eeaf4e87ba7ef308d1d9
jeel narola <narolajeel29@gmail.com>
2023-08-02 12:29:44 +0530

    student data

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+const disply=((data)=>{
+    console.log(data)
+    data.map((item)=>{
+        let tr=document.createElement("tr")
+        let td1=document.createElement("td")
+        td1.innerHTML=item.username
+        let td2=document.createElement("td")
+        td2.innerHTML=item.password
+        let td3=document.createElement("button")
+        td3.innerHTML="DELETE"
+        td3.setAttribute("class","button")
+        td3.addEventListener("click",()=>{
+            a(item.id);
+            // console.log(item.id)
+        })
+        let btn=document.createElement("button")
+      
+        tr.append(td1,td2,td3)
+        document.getElementById("disply").append(tr)
+    })
+
+})
+
+
+let a = async (id) => {
+    fetch(`http://localhost:8090/data/${id}`, {
+        method: "DELETE"
+    })
+
+}
+
+
+document.querySelector("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let data={
+        username:document.getElementById("username").value,
+        password:document.getElementById("password").value
+    }
+    console.log(data)
+
+    fetch("http://localhost:8090/data",{
+        method:"POST",
+        headers: { "Content-Type": "application/json" },
+        body:JSON.stringify(data)
+    })
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
+})
+
+
+
+fetch("http://localhost:8090/data")
+.then((res)=>res.json())
+.then((data)=>disply(data))
